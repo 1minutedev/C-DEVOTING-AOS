d084c750f273b1a3ee47be73803d704d24a21044
jwonmin4 <jwonmin4@gmail.com>
2019-11-08 00:22:42 +0900

    no message

--- CVoting/C-Voting/src/main/java/com/kkj/cvoting/view/fragment/DiscussionFragment.java
+++ CVoting/C-Voting/src/main/java/com/kkj/cvoting/view/fragment/DiscussionFragment.java
@@ -1,4 +1,44 @@
 package com.kkj.cvoting.view.fragment;
 
-public class DiscussionFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.kkj.cvoting.R;
+import com.kkj.cvoting.view.MainFragmentActivity;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DiscussionFragment extends Fragment {
+    private View wrapper;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        wrapper = inflater.inflate(getResources().getLayout(R.layout.fragment_discussion), null);
+        return wrapper;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        wrapper.setClickable(true);
+    }
+
+    private void goToFragment(Fragment fragment) {
+        getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(getResources().getIdentifier("anim_slide_in_left", "anim", getActivity().getPackageName()), getResources().getIdentifier("hold", "anim", getActivity().getPackageName()))
+                .add(getResources().getIdentifier("content_frame", "id", getActivity().getPackageName()), fragment, null)
+                .commitAllowingStateLoss();
+        MainFragmentActivity.addFragment(fragment);
+    }
 }
